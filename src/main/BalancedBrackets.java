a752a3a297ffbda05760a2492943aa6ced90cd26
Daniel C <daniel@daniel.com>
2019-11-26 14:37:55 -0600

    main studio finished, committing before starting bonus

--- src/main/BalancedBrackets.java
+++ src/main/BalancedBrackets.java
@@ -1,6 +1,5 @@
 package main;
 
-
 public class BalancedBrackets {
     /**
      * The function BalancedBrackets should return true if and only if
@@ -20,16 +19,29 @@ public class BalancedBrackets {
      *
      * @param str - to be validated
      * @return true if balanced, false otherwise
-     */
+     *
+     **/
+
+    //  unbalanced test strings   "][", "]", "Launch]Code[", "[LaunchCode", "]La,La, La, [Love] is all you need["
+    // 4 output lines to show ch found and brackets current value for debugging, uncomment if needed.
+
     public static boolean hasBalancedBrackets(String str) {
         int brackets = 0;
         for (char ch : str.toCharArray()) {
             if (ch == '[') {
                 brackets++;
+                //System.out.println(" Ordered Left Bracket '[' found, Brackets = " + brackets + " ch = " + ch);
             } else if (ch == ']') {
                 brackets--;
+                //System.out.println(" Ordered Right Bracket ']' found, Brackets = " + brackets + " ch = " + ch);
+            }
+
+            if (brackets == -1) {
+                //System.out.println("Unordered Bracket '][' came first, Brackets = " + brackets + " ch = " + ch);
+                break;
             }
         }
+        //System.out.println("Loop finished. Final value of brackets is: " + brackets);
         return brackets == 0;
     }
 }
--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,6 +11,36 @@ public class BalancedBracketsTest {
     @Test
     public void emptyTest() {
         assertEquals(true, true);
+    }
+/*
+hese strings have balanced brackets:
+     *  "[LaunchCode]", "Launch[Code]", "[]LaunchCode", "", "[]"
+     *
+     * While these do not:
+     *   "[LaunchCode", "Launch]Code[", "[", "]["
+ */
+    @Test
+    public void BracketsReturnsTrue() {
+        BalancedBrackets test_Class = new BalancedBrackets();
+        assertTrue(test_Class.hasBalancedBrackets(""));
+        assertTrue(test_Class.hasBalancedBrackets(" "));
+        assertTrue(test_Class.hasBalancedBrackets("[]"));
+        assertTrue(test_Class.hasBalancedBrackets("[]LaunchCode"));
+        assertTrue(test_Class.hasBalancedBrackets("Launch[Code]"));
+        assertTrue(test_Class.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    @Test
+    public void unmatchedBracketsReturnsFalse() {
+        BalancedBrackets test_Class = new BalancedBrackets();
+        assertFalse(test_Class.hasBalancedBrackets("]["));
+        assertFalse(test_Class.hasBalancedBrackets("["));
+        assertFalse(test_Class.hasBalancedBrackets("]"));
+        assertFalse(test_Class.hasBalancedBrackets("Launch]Code["));
+        assertFalse(test_Class.hasBalancedBrackets("[Launch[Code]"));
+        assertFalse(test_Class.hasBalancedBrackets("[LaunchCode["));
+        assertFalse(test_Class.hasBalancedBrackets("[LaunchCode"));
+        assertFalse(test_Class.hasBalancedBrackets("]La,La, La, [Love] is all you need["));
     }
 
 
